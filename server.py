9911810e88ef0b285f09bb0c4cebb18f07ffed9a
jviaud0 <jviaud.ieu2021@student.ie.edu>
2023-10-18 06:54:03 +0200

    added server introduction

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
 import random as rd
 
 class Server:
-    def __init__(self, hands):
-        self.hands = rd.randint(1,3)
+    def __init__(self,hands, name):
+        self.name = name
+        self.hands = hands
+    def introduce(self):
+        print(f"Hello, my name is {self.name} and I have {self.hands} hands."
+              f"\nI will be your server for today")
+
+Chad = Server(hands = rd.randint(1,3), name = "Chad", )
+Chad.introduce()
+
+
+
+
+
 
