4d339ab797e3924c6a203e5ebf2efe14f091bc32
falba3 <99144557+falba3@users.noreply.github.com>
2023-10-25 15:24:21 +0200

    franks script

---
+++ franks/frank_script.py
@@ -0,0 +1,9 @@
+
+class Bar:
+    def __init__(self, name: str, servers):
+        self.name = name
+        print(f"{name} just opened! ")
+        self.servers = servers
+
+
+CienMontaditos = Bar("100 Montaditos")
