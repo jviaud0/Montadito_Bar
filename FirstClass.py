7623dbd55bd9d670c6b4c092e5f5a74615353809
falba3 <99144557+falba3@users.noreply.github.com>
2023-11-03 21:42:05 +0100

    zombie simulation added

--- FirstClass.py
+++ FirstClass.py
@@ -1,18 +1,18 @@
 
 
-# class Aircraft:
-#     def __init__(self, color, size, manufacturer):
-#         self.color = color
-#         self.size = size
-#         self.manufacturer = manufacturer
-#
-#     def fly(self):
-#         print(f"The {self.size}-sized {self.color} {self.manufacturer} plane took off! ")
-#
-#
-#
-# giannis_plane = Aircraft('Blue', 'Medium', 'Boeing')
-# giannis_plane.fly()
+class Aircraft:
+    def __init__(self, color, size, manufacturer):
+        self.color = color
+        self.size = size
+        self.manufacturer = manufacturer
+
+    def fly(self):
+        print(f"The {self.size}-sized {self.color} {self.manufacturer} plane took off! ")
+
+
+
+giannis_plane = Aircraft('Blue', 'Medium', 'Boeing')
+giannis_plane.fly()
 
 
 
