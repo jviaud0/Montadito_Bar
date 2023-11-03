d9ca81c44cbbef8a481fe2c008ddc03f0c6e47e4
Gianfranco-90 <gianfrancoptorres@gmail.com>
2023-11-03 21:29:30 +0100

    Commit

--- FirstClass.py
+++ FirstClass.py
@@ -1,1 +1,10 @@
-a
+class aircraft:
+    def __init__(self, manufacturer):
+        self.manufacturer = manufacturer
+    def welcome(self):
+        print(f"Welcome to {self.name}")
+
+
+
+shout = Bar(name = 'Shout')
+shout.welcome()
